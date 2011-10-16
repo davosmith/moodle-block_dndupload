df0088c4792e04d112eed4346f6c01f676d1b509
Davo Smith <git@davosmith.co.uk>
2011-10-16 19:19:33 +0100

    Added Spanish translation - thanks to German Valero

---
+++ lang/es_utf8/block_dndupload.php
@@ -0,0 +1,35 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://132.248.50.7
+ *
+ * @package    block
+ * @subpackage dndupload
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addhere'] = 'Añadir archivo(s) aquí';
+$string['blockname'] = 'Arrastrar y depositar archivos';
+$string['dndworking'] = 'Arrastre y deposite archivos desde el escritorio hasta secciones del curso para SUBIR ARCHIVOS a MOODLE';
+$string['filetoolarge'] = 'es demasiado grande para cargarlo';
+$string['noajax'] = 'Debe habilitarse la edicion de cursos con AJAX para que funcione "Arrastrar y depositar" para subir archivos';
+$string['nofilereader'] = 'Su navegador no soporta la subida de archivos mediante arrastrar y depositar';
+$string['noscript'] = 'Debe habilitarse Javascript para que funcione "Arrastrar y depositar" para subir archivos';
+$string['pluginname'] = 'Arrastrar y depositar archivos';
