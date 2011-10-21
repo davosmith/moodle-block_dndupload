7595cbe2c942842037df838bbf95e2e551117526
Davo Smith <git@davosmith.co.uk>
2011-10-21 19:17:56 +0100

    Added Italian translation - thanks to Marco Cassisa

---
+++ lang/it_utf8/block_dndupload.php
@@ -0,0 +1,25 @@
+<?php
+
+// This file is part of the Drag and drop upload block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['addhere'] = 'Aggiungi file(s) qui';
+$string['blockname'] = 'Drag and drop upload';
+$string['dndworking'] = 'Trascina e rilascia (Drag and drop) un file dal tuo PC sulle sezioni del corso per caricarle sul server';
+$string['filetoolarge'] = 'è troppo largo per farne l\'upload';
+$string['noajax'] = 'Deve essere abilitato l\'editing AJAX del corso per poter effettuare l\'upload';
+$string['nofilereader'] = 'Il tuo browser non supporta l\'upload (non usare Internet Explorer)';
+$string['noscript'] = 'Javascript deve essere abilitato per poter effettuare l\'upload';
+$string['pluginname'] = 'Drag and drop upload';
