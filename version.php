61891af311cb7cff79d399e8c9abc3aeb7314ccd
Davo Smith <git@davosmith.co.uk>
2011-10-21 19:18:52 +0100

    Bumped version number

--- version.php
+++ version.php
@@ -15,5 +15,5 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version = 2011101600;
+$plugin->version = 2011102100;
 
