f02a9c98c2b43c8ae5c14f38fec542bd7d48fffc
Davo Smith <git@davosmith.co.uk>
2011-11-14 19:17:45 +0000

    Added Czech translation from Bohumil Havel

---
+++ lang/cz/block_dndupload.php
@@ -0,0 +1,38 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://192.168.1.204/moodlebohous21
+ *
+ * @package    block
+ * @subpackage dndupload
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['addhere'] = 'Zde přidej soubor(y)';
+$string['addlinkhere'] = 'Přidej odkaz';
+$string['addwebpagehere'] = 'Přidej stránku';
+$string['blockname'] = 'Nahrání Drag and drop';
+$string['dndworking'] = 'Přetažení souborů, textu nebo odkazů z počítače do kurzu';
+$string['filetoolarge'] = 'příliš velké pro nahrání';
+$string['nameforlink'] = 'Jak chcete tento odkaz nazvat?';
+$string['nameforwebpage'] = 'Jak chcete nazvat tuto stránku?';
+$string['noajax'] = 'Aby pracovalo nahrání přetažením, musí být povolena úprava kurzu AJAX';
+$string['nofilereader'] = 'Váš prohlížeč nepodporuje nahrání souborů přetažením';
+$string['noscript'] = 'Aby pracovalo nahrání přetažením, musí být povolen Javascript';
+$string['pluginname'] = 'Drag and drop upload';
