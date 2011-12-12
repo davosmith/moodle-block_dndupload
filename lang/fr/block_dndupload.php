7d74659dfd61f34cdb73ec39d7f8d81ed56a814b
Davo Smith <git@davosmith.co.uk>
2011-12-12 18:44:14 +0000

    Added French translation from Mary Cooch

---
+++ lang/fr/block_dndupload.php
@@ -0,0 +1,29 @@
+<?php
+
+// This file is part of the Drag and drop upload block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['addhere'] = 'Ajoutez un(des)fichier(s)ici';
+$string['addlinkhere'] = 'Ajoutez un lien ici';
+$string['addwebpagehere'] = 'Ajoutez une page ici';
+$string['blockname'] = 'Dépôt de fichiers par glisser-déposer';
+$string['dndworking'] = 'Déplacez des fichiers, du texte ou des liens du Bureau vers vos sections de cours par glisser-déposer';
+$string['filetoolarge'] = 'est trop lourd pour déposer';
+$string['nameforlink'] = 'Quel nom donnez-vous à ce lien?';
+$string['nameforwebpage'] = 'Quel nom donnez-vous à cette page?';
+$string['noajax'] = 'Pour que le glisser-déposer fonctionne, l\'édition Ajax doit être activée';
+$string['nofilereader'] = 'Votre navigateur ne supporte pas le dépôt de fichiers par glisser-déposer';
+$string['noscript'] = 'Pour que le glisser-déposer fonctionne, le javascript doit être activé';
+$string['pluginname'] = 'Dépôt de fichiers par glisser-déposer';
