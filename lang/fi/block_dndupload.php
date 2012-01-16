a62a358392eaea968d95a237af8b318371b2613f
Davo Smith <git@davosmith.co.uk>
2012-01-16 18:16:18 +0000

    Added Finnish / Dutch translations from Paul Nijbakker

---
+++ lang/fi/block_dndupload.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+// This file is part of the Drag and drop upload block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['addhere'] = 'Lisää tiedosto(t) tänne';
+$string['addlinkhere'] = 'Lisää linkki tänne';
+$string['addwebpagehere'] = 'Lisää websivu tänne';
+$string['blockname'] = 'Vedä ja pudota -lataus';
+$string['dndworking'] = 'Vedä ja pudota tiedostoja, tekstiä tai linkkejä tietokoneen työpöydältäsi moodlekurssin osalle ja lataa se siten';
+$string['filetoolarge'] = 'on liian suuri tiedosto';
+$string['nameforlink'] = 'Anna linkin nimi';
+$string['nameforwebpage'] = 'Anna sivun nimi';
+$string['noajax'] = 'AJAX ei ole päälle, ilmoita Moodleylläpitäjälle';
+$string['nofilereader'] = 'Selaimesi ei voi vetää ja pudottaa tiedostoa';
+$string['nopermission'] = 'Sinulla ei ole lupa vetää ja pudottaa tiedostoa';
+$string['noscript'] = 'Tarvitaan Javascript, että voit vetää ja pudottaa tiedostoja';
+$string['pluginname'] = 'Vedä ja pudota -lataus';
---
+++ lang/nl/block_dndupload.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+// This file is part of the Drag and drop upload block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['addhere'] = 'Sleep file(s) hier naartoe';
+$string['addlinkhere'] = 'Sleep link hier naartoe';
+$string['addwebpagehere'] = 'Sleep pagina hier naartoe';
+$string['blockname'] = 'Drag and drop upload';
+$string['dndworking'] = 'Sleep files, tekst of links van je desktop naar cursussecties in Moodle en laad ze zo op';
+$string['filetoolarge'] = 'is te groot om op te laden';
+$string['nameforlink'] = 'Hoe wil je deze link noemen?';
+$string['nameforwebpage'] = 'Hoe wil je deze pagina noemen?';
+$string['noajax'] = 'AJAX moet ingeschakeld zijn anders werkt drag and drop upload niet';
+$string['nofilereader'] = 'Je browser ondersteunt het slepen van files niet';
+$string['nopermission'] = 'Je hebt geen toestemming files naar deze cursus op te laden';
+$string['noscript'] = 'Javascript moet geactiveerd zijn anders werkt drag and drop upload niet';
+$string['pluginname'] = 'Drag and drop upload';
