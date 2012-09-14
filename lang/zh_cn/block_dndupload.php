bae43bf039b2c8cad0d37aa12a600cbd8ab062ee
Davo Smith <git@davosmith.co.uk>
2012-09-14 18:57:02 +0100

    Added Chinese translation

---
+++ lang/zh_cn/block_dndupload.php
@@ -0,0 +1,32 @@
+<?php
+
+// This file is part of the Drag and drop upload block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+$string['addhere'] = '在这里添加文件';
+$string['addlinkhere'] = '在这里添加链接';
+$string['addwebpagehere'] = '在这里添加网页';
+$string['blockname'] = '文件拖动上传';
+$string['dndworking'] = '通过从本地计算机中拖动文件，文本或者链接到课程相应主题中上传文件';
+$string['filetoolarge'] = '文件太大无法上传';
+$string['nameforlink'] = '链接名称?';
+$string['nameforwebpage'] = '网页名称?';
+$string['noajax'] = 'AJAX 课程编辑功能必须启动';
+$string['nofilereader'] = '您的浏览器不支持此功能';
+$string['nopermission'] = '您没有添加文件到此课程中的权限';
+$string['noscript'] = 'Javascript 必须启动';
+$string['notmoodle23'] = '您的站点是moodle2.3或以上版本，已经具备此项功能，联系管理员删除此功能';
+$string['pluginname'] = '拖动文件上传';
